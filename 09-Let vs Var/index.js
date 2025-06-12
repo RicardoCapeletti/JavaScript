6baed89009d073a56de8194bb5ba8400a185312e
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-12 12:43:43 -0300

    commit-09

---
+++ 09-Let vs Var/index.js
@@ -0,0 +1,16 @@
+//var pode ser redeclarada
+var nome = 'Ricardo';
+var nome = 'Capeletti';
+
+//let não pode ser redeclarada
+let idade = 37;
+let idade = 21; // Isso vai gerar um erro
+
+//Tipos de dados primitivos
+// string, number, undefined, null, boolean
+const nomeCompleto = 'Ricardo Capeletti'; // string
+const peso = 80; // number
+const alturaEmM = 1.80; // number
+let imc; //undefined
+const anoAtual = null; // nulo
+const aprovado = true; // boolean
