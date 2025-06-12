19418c0f2b7dbcd193849439ffcc349f30abc231
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-12 10:39:34 -0300

    commit-04

--- 03-Console Log/index.js
+++ 03-Console Log/index.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!");
+console.log("'Hello, World!'");
+console.log('"Hello, World!"');
+console.log(`'"Hello, World!"'`);
+
+console.log(123456, 12.34, 'Texto');
+
+console.log('Meu nome é "Ricardo". Estou aprendendo JavaScript às', 10, 'da manhã.');
---
+++ 04-Comentarios/index.js
@@ -0,0 +1,11 @@
+// Comentarios em JavaScript
+console.log("Hello, World!"); // Este é um comentário de linha única
+
+
+/* comentario de
+varias linhas
+de texto */
+console.log("linha 1");
+// console.log("linha 2");
+console.log("linha 3");
+
