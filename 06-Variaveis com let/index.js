a67175aa5e990035fd893a64d105c193da3ca738
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-12 11:07:31 -0300

    commit-06

---
+++ 06-Variaveis com let/index.js
@@ -0,0 +1,12 @@
+//Não podemos criar variáveis com palavras reservadas
+//Variáveis precisam ter nomes significativos
+//Não podemos começar o nome de uma variável com um número
+//Não podem conter espaços ou traços
+//Utilizamos camelCase
+//Não podemos redeclarar variáveis com let
+//Não utilizamos var, apenas let e const
+let nomeCliente = 'Ricardo'; // String
+console.log(nomeCliente); // Saída: João
+let idadeCliente = 30; // Number
+idadeCliente = 37; // Reatribuição de valor
+console.log(idadeCliente); // Saída: 37
