7c5c8fb4102f784c2eadbd08aa4caf7a8b0af376
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-12 18:43:51 -0300

    commit-17

---
+++ 17-Funcoes/index.js
@@ -0,0 +1,18 @@
+// Exemplo de função simples em JavaScript
+function soma(a, b) {
+    return a + b;
+}
+
+console.log(soma(5, 3));
+
+// Exemplo de função com parâmetros padrão
+const raiz = function (x) {
+    return x ** 0.5;
+}
+
+console.log(raiz(9));
+
+// Exemplo de uma aerow function
+const raiz2 = n => n ** 0.5;
+
+console.log(raiz2(16));
