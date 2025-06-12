59413f6a70e584b11be82bb63b777e0ec52dc462
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-12 11:20:53 -0300

    commit-07

---
+++ 07-Constantes com const/index.js
@@ -0,0 +1,12 @@
+//Não podemos criar constantes com palavras reservadas
+//Constantes precisam ter nomes significativos
+//Não podemos começar o nome de uma constante com um número
+//Não podem conter espaços ou traços
+//Utilizamos o padrão camelCase
+//Case-sensitive
+//Não podemos redeclarar constantes
+//Não utilizamos var ou let para declarar constantes
+
+const nome = 'João'; // String
+const idade = 30; // Number
+console.log(nome, idade);
