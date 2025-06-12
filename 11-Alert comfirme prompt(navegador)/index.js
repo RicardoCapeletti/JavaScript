594c9adbd6fcfd4f28abd26213fa2e592892c830
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-12 13:35:14 -0300

    commit-11

---
+++ 11-Alert comfirme prompt(navegador)/index.js
@@ -0,0 +1,5 @@
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+
+alert(`Você digitou os números ${num1} e ${num2}.`);
+alert(`A soma é: ${Number(num1) + Number(num2)}.`);
