31e442c57aad59c95e9ed1a1b8e2a8fc2f1788ff
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-12 19:51:33 -0300

    commit-18

---
+++ 18-Objetos/index.js
@@ -0,0 +1,29 @@
+/* function criaPessoa(nome, sobrenome, idade) {
+  return {
+    nome: nome,
+    sobrenome: sobrenome,
+    idade: idade,
+    
+    };
+  } */
+
+
+  /* function criaPessoa(nome, sobrenome, idade) {
+  return { nome, sobrenome, idade };
+  }
+
+  const pessoa = criaPessoa('Paulo', 'Vieira', 40);
+  const pessoa1 = criaPessoa('Ricardo', 'Capeletti', 37);
+  const pessoa2 = criaPessoa('Luiza, ', 'Capeletti', 27); */
+
+const pessoa = {
+  nome: 'Paulo',
+    sobrenome: 'Vieira',
+    idade: 40,
+
+    fala() {
+      console.log(`${this.nome} ${this.sobrenome} está falando`);
+    },
+};
+
+  pessoa.fala();
