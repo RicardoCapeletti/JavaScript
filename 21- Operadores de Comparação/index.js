937c3269f2ce83d3e43401ccf75ab6cf3637bcf8
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-16 11:17:04 -0300

    commit-21

---
+++ 21- Operadores de Comparação/index.js
@@ -0,0 +1,25 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+*/
+console.log(10 > 5);
+
+console.log(7 >= 5);
+
+console.log(10 < 2);
+
+console.log(2 <= 2);
+
+console.log(2 <= 2);
+
+
+
+const num1 = 10; // number
+const num2 = 10; // number
+const comp = num1 !== num2;
+console.log(comp);
