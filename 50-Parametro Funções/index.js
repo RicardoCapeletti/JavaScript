0fe15f4b60e14c1ddfea1f1b931785eb792d793d
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-23 17:52:36 -0300

    commit-50

--- 50-Parametro Funções/index.js
+++ 50-Parametro Funções/index.js
@@ -1,4 +1,22 @@
-function funcao (a, b, c = 5) {
-   console.log(a + b + c);
+function recebeDados () {
+    let nome = document.getElementById("nome").value;
+    let sobrenome = document.getElementById("sobrenome").value;
+    let idade = document.getElementById("idade").value;
+    return {nome, sobrenome, idade};
 }
-funcao(1, 2);
+
+function criaObjetos (nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    };
+}
+
+function mostraPessoa () {
+    let dados = recebeDados();
+    let pessoa = criaObjetos(dados.nome, dados.sobrenome, dados.idade);
+    let dadosPessoa = `${pessoa.nome} ${pessoa.sobrenome} ${pessoa.idade}`;
+
+    document.getElementById("resultado").innerHTML = `${dadosPessoa}`;
+}
