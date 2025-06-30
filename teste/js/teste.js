51103828a8ba5bdfd8e00594f5f0db3f902e6a5f
Ricardo Capeletti <166241408+RicardoCapeletti@users.noreply.github.com>
2025-06-30 14:31:44 -0300

    commit85

--- teste/js/teste.js
+++ teste/js/teste.js
@@ -1,13 +1,45 @@
+//------------------------FACTORY----------------------------//
+
 function criaObjeto (nome, idade) {
     return {
         nome,
         idade,
 
         mostra: function () {
-            console.log(`Nome: ${nome}, Idade: ${idade}`);
+            console.log(`Nome: ${nome} Idade: ${idade}`);
         }
     };
 }
 
-const princesa = criaObjeto('Luna', 18);
-princesa.mostra();
+const namo = criaObjeto ('Luna', 18);
+namo.mostra();
+
+//------------------------CONSTRUCTOR---------------------------//
+
+function CriaObjeto2 (nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+}
+
+CriaObjeto2.prototype.mostra2 = function () {
+    console.log(`Nome: ${this.nome} Idade: ${this.idade}`);
+}
+
+const namo2 = new CriaObjeto2 ('Priscila', 19);
+namo2.mostra2();
+
+//-------------------------CLASS----------------------------//
+
+class Objeto3 {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    mostra3() {
+        console.log(`Nome: ${this.nome} Idade: ${this.idade}`);
+    }
+}
+
+const namo3 = new Objeto3(`Camila`, 20);
+namo3.mostra3();
